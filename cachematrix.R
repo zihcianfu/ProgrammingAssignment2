5071f0aeec3f67a3f0ca1de1b127e34af7c78f9d
zihcianfu <96919117+zihcianfu@users.noreply.github.com>
2022-01-02 23:10:25 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## n: Create an n*n matrix
+install.packages("matlib")
+library(matlib)
+makeCacheMatrix <- function(n) {
+  vec <- sample(1:100, n*n, replace=TRUE)
+  A <- matrix(vec, nrow=n, ncol=n)
+  if (det(A) != 0) {
+    AI  <- inv(A)
+    list_a_ai <- list(A=A,AI=AI) 
+    return(list_a_ai)
+  }else{
+    print('the matrix is not invertible')
+  }
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), then cacheSolve should retrieve the inverse from the cache.
+## inputlist: list contains matrix & inverse matrix form makeCacheMatrix
+cacheSolve <- function(inputlist) {
+  testA <- inputlist$A
+  testAI  <- inputlist$AI
+  if (all.equal(inv(testAI),testA,tolerance=1e-6)) {
+    inverse_mat <- inputlist$AI
+    return(inverse_mat)
+  }else{
+    print('the matrix has changed')
+  }
 }
